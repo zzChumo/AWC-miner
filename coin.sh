1cca6d7a7121ddc924520133dd4e9c42fe837829
zzChumo <95031304+zzChumo@users.noreply.github.com>
2023-08-04 17:02:52 +0800

    Update coin.sh

--- coin.sh
+++ coin.sh
@@ -11,7 +11,8 @@ curl -sd "account=$ACC&password=$PWD" https://coin.awa.ac.cn/api/v1/mine/create 
 HASH=$(cat c.json | jq .hash -r)
 SALT=$(cat c.json | jq .salt -r)
 ID=$(cat c.json | jq .id -r)
-hashcat -a 3 -m 1710 "$HASH:$SALT" "?d?d?d?d?d?d?d?d" --increment --increment-min 1 --increment-max 9 -O -o t.txt --outfile-format=2 > /dev/null
+#hashcat -a 3 -m 1710 "$HASH:$SALT" "?d?d?d?d?d?d?d?d" --increment --increment-min 1 --increment-max 9 -O -o t.txt --outfile-format=2 > /dev/null
+hashcat -a 3 -m 1710 "$HASH:$SALT" "?d?d?d?d?d?d?d?d" -O -o t.txt --outfile-format=2 > /dev/null
 ANS=$(cat t.txt)
 curl -sd "id=$ID&answer=$ANS" https://coin.awa.ac.cn/api/v1/mine/finish > e.json
 BALANCE=$(cat e.json | jq .balance -r)
